1c5bac77c04231dba398004c2fd203383746c733
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-06-29 01:59:33 -0300

    feat: add a global styles, theme and refact initial code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Details } from "./pages/Details/index.jsx"
+import { ThemeProvider } from "styled-components"
+import theme from "./styles/theme.js"
+import GlobalStyles from "./styles/global.js"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
 )
---
+++ src/pages/Details/style.js
@@ -0,0 +1,8 @@
+import { styled } from "styled-components"
+
+export const Wrapper = styled.div`
+font-size: 1rem;
+text-align: center;
+color: #f1f1f1;
+background: unset;
+`
