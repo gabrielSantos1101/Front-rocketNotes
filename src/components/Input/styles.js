1537ae1a9e1d5425e12e23413225b806dbf504b0
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-07-03 23:55:44 -0300

    style: new style input

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,4 +3,11 @@ import { styled } from 'styled-components'
 export const InputStyle = styled.input`
   border-radius: 10px;
   background: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  width: 100%;
+  height: 56px;
+
+  :placeholder-shown {
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+    font-size: 1rem;
+  }
 `
