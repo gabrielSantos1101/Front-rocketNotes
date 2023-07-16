914c38a761794b3b7698ac13e3d79f919091126e
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-07-15 23:31:46 -0300

    chore: completed Singin screen

--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,16 +1,22 @@
 import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
+import { TextButton } from '../../components/TextButton'
 import { Wrapper } from './styles'
 export function SingIn() {
   return (
     <Wrapper>
-      <div className="singIn">
+      <div className="content">
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
         <form action="">
+          <h2>faça seu login</h2>
+
           <Input Type="email" placeholder="E-mail" />
           <Input Type="password" placeholder="Senha" />
-          <Button title="Entrar" radius />
-          <Button title="teste" />
+          <Button title="Entrar" round={true} />
         </form>
+
+        <TextButton title="Criar conta" />
       </div>
       <img
         src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
--- src/pages/SingIn/styles.js
+++ src/pages/SingIn/styles.js
@@ -2,17 +2,56 @@ import { styled } from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
+  align-items: center;
+  height: 100%;
+  width: 100%;
 
-  & .singIn {
-    width: 40%;
+  & .content {
+    display: flex;
+    width: 35%;
+    flex-direction: column;
+    width: 100%;
 
-    padding-inline: 10rem;
+    & h1 {
+      text-align: center;
+      font-size: 3rem;
+      color: ${({ theme }) => theme.COLORS.ORANGE};
+    }
+
+    & p {
+      text-align: center;
+      font-size: 0.875rem;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+
+      margin-bottom: 3rem;
+    }
+
+    & form {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+
+      padding-inline: 10rem;
+      margin-bottom: 7.75rem;
+
+      & h2 {
+        text-align: center;
+        font-size: 1.5rem;
+
+        margin-bottom: 2.5rem;
+      }
+
+      & button {
+        margin-top: 1.5rem;
+      }
+    }
   }
 
   & img {
-    width: 100%;
+    width: 65%;
     height: 100%;
     object-fit: cover;
     filter: brightness(0.4);
+    opacity: 0.5;
   }
 `
