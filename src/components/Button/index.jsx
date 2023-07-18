d9066de314d745d15d1c58388338a7ab4d34419d
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-07-17 23:51:58 -0300

    fix: styledcomponents error on Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import { Wrap } from './styles'
 
 export function Button({ title, loading = false, round = false }) {
   return (
-    <Wrap type="button" disabled={loading} round={round.toString()}>
+    <Wrap type="button" disabled={loading} $round={round.toString()}>
       {title}
     </Wrap>
   )
