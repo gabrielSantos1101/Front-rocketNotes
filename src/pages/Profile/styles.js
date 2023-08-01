6ffd566363223fe1c64e0c43e72dc38dee98ec32
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-07-31 23:25:38 -0300

    style: add style for avatar image

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -92,3 +92,48 @@ export const Wrapper = styled.div`
     }
   }
 `
+export const Wrap = styled.div`
+  width: fit-content;
+  height: fit-content;
+  position: relative;
+
+  margin-inline: auto;
+  margin-bottom: 4rem;
+
+  & img {
+    width: 11.25rem;
+    height: 11.25rem;
+    object-fit: cover;
+    border-radius: 100%;
+  }
+
+  & span {
+    display: grid;
+    place-items: center;
+    width: 3rem;
+    height: 3rem;
+
+    position: absolute;
+    bottom: 0;
+    right: 0;
+
+    cursor: pointer;
+    border-radius: 100%;
+    background-color: ${({ theme }) => theme.COLORS.ORANGE};
+
+    &:hover {
+      & svg path {
+        color: ${({ theme }) => theme.COLORS.WHITE};
+      }
+    }
+
+    & svg path {
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    }
+  }
+
+  & input {
+    width: 0;
+    height: 0;
+  }
+`
